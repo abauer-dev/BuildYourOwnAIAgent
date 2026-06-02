c234dbcde9180a15df115311f1d62007ce377d65
alexb <alexander.bauer.br@gmail.com>
2026-06-02 13:08:54 +0200

    added write file capability

---
+++ functions/write_file.py
@@ -0,0 +1,19 @@
+import os
+
+def write_file(working_directory: str, file_path: str, content: str) -> str:
+    try:
+        #Workpath validation
+        working_dir_abs = os.path.abspath(working_directory)
+        target_path = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        valid_target_path = os.path.commonpath([working_dir_abs, target_path]) == working_dir_abs
+        if not valid_target_path:
+            raise Exception(f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory')
+        if os.path.isdir(target_path):
+            raise Exception(f'Error: Cannot write to "{file_path}" as it is a directory')
+        os.makedirs(file_path,exist_ok=True)
+
+        with open(target_path, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return e
